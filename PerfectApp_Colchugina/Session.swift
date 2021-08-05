a3a26580f9c1a5029efd6e828a144cb42fafaaa8
Iren <iren19098@gmail.com>
2021-08-05 11:51:30 +0300

    added session file

--- PerfectApp_Colchugina/Session.swift
+++ PerfectApp_Colchugina/Session.swift
@@ -2,17 +2,15 @@
 //  Session.swift
 //  PerfectApp_Colchugina
 //
-//  Created by Ирина Кольчугина on 22.06.2021.
+//  Created by Ирина Кольчугина on 05.08.2021.
 //
 
 import Foundation
 
-class Session {
-    static var shared = Session()
-    
+final class Session {
+    private static var shared = Session()
+    private var id: Int?
+    private var token: String?
     private init() {}
-    
-    var token: String?
-    var id: Int?
-
 }
+
